b7ca5aa11bf85cf15f2dd9f39aae609a3f69f330
Luca Kiebel <github@luca-kiebel.de>
2022-09-02 09:49:53 +0200

    remove invoice information

--- src/invoice-information/dto/update-invoice-information.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateInvoiceInformationDto } from './create-invoice-information.dto';
-
-export class UpdateInvoiceInformationDto extends PartialType(CreateInvoiceInformationDto) {}
